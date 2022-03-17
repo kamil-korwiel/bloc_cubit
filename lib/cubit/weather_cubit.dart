ceb39a03f5f8db350897cdcd1c77563a1df99ee6
kamil-korwiel <kamilkorwiel@gmail.com>
2022-03-17 15:47:43 +0100

    end Cubit weather example

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_cubit/weather_model.dart';
+import 'package:bloc_cubit/weather_repository.dart';
 import 'package:equatable/equatable.dart';
 
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  final WeatherRepository _weatherRepository;
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+
+  Future<void> getWeather() async {
+    try{
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather("Tor");
+      emit(WeatherLoaded(weather));
+    }on Exception catch (e) {
+      emit(WeatherError(e.toString()));
+    }
+  }
 }
--- lib/cubit/weather_state.dart
+++ lib/cubit/weather_state.dart
@@ -1,10 +1,31 @@
 part of 'weather_cubit.dart';
 
 abstract class WeatherState extends Equatable {
-  const WeatherState();
 
   @override
   List<Object> get props => [];
 }
 
-class WeatherInitial extends WeatherState {}
+class WeatherInitial extends WeatherState {
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherLoading extends WeatherState {
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+  WeatherLoaded(this.weather);
+  @override
+  List<Object> get props => [weather];
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+  WeatherError(this.message);
+  @override
+  List<Object> get props => [message];
+}
